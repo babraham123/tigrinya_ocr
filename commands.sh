909c929f3dc0e58118f6269837c97bdf643e660c
Bereket Abraham <babraham42@gmail.com>
2018-04-09 02:38:17 -0700

    tesstrain

--- commands.sh
+++ commands.sh
@@ -22,6 +22,26 @@ combine_lang_model --input_unicharset ~/tigrinya_ocr/tir/Ethiopic.unicharset \
   --words ~/tigrinya_ocr/tir/tir.wordlist --puncs ~/tigrinya_ocr/tir/tir.punc --numbers ~/tigrinya_ocr/tir/tir.numbers \
   --version_str 0.0.1
 
+lstmtraining --model_output ~/tigrinya_ocr/training1 [--max_image_MB 6000] \
+  --traineddata ~/tigrinya_ocr/training/tir/tir.traineddata \
+  --train_listfile /path/to/list/of/filenames.txt
+
+export TESSDATA_PREFIX=/home/babraham/tigrinya_ocr/training
+
+fc-list | grep tir_fonts
+
+"Ethiopic WashRa SemiBold,Ethiopic Yigezu Bisrat Gothic,Ethiopic Tint,Code2003,Ethiopic Yebse,Ethiopic Zelan,Ethiopic Wookianos,Abyssinica SIL,Abyssinica SIL Zaima,GF Zemen Unicode,Abyssinica SIL Sebatbeit,Ethiopic Fantuwua,Ethiopic Yigezu Bisrat Goffer,Ethiopia Jiret,Ethiopic WashRa Bold,Ethiopic Hiwua,Abyssinica SIL Ximtanga"
+
+tesstrain.sh \
+  --fontlist "Ethiopic WashRa SemiBold,Ethiopic Yigezu Bisrat Gothic,Ethiopic Tint,Code2003,Ethiopic Yebse,Ethiopic Zelan,Ethiopic Wookianos,Abyssinica SIL,Abyssinica SIL Zaima,GF Zemen Unicode,Abyssinica SIL Sebatbeit,Ethiopic Fantuwua,Ethiopic Yigezu Bisrat Goffer,Ethiopia Jiret,Ethiopic WashRa Bold,Ethiopic Hiwua,Abyssinica SIL Ximtanga" \
+  --fonts_dir /usr/local/share/fonts/tir_fonts \
+  --lang tir --langdata_dir ~/tigrinya_ocr \
+  --output_dir ~/tigrinya_ocr/training1 \
+  --overwrite --linedata_only --exposures 0 \
+  --training_text ~/tigrinya_ocr/eval/tir_groundtruth.txt \
+  --wordlist ~/tigrinya_ocr/tir/tir.wordlist \
+  --tessdata_dir ~/tigrinya_ocr/training
+
 lstmtraining --model_output /path/to/output [--max_image_MB 6000] \
   --continue_from /path/to/existing/model \
   --traineddata /path/to/original/traineddata \
@@ -34,7 +54,7 @@ lstmeval --model ~/training/impact_checkpoint \
   --eval_listfile ~/tesstutorial/engeval/eng.training_files.txt
 
 
-export TESSDATA_PREFIX=/home/babraham/tigrinya_ocr/tessdata/best
+export TESSDATA_PREFIX=/home/babraham/tigrinya_ocr/training
 java -jar VietOCR.jar
 
 
