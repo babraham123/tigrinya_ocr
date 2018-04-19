053765bed132754335cd281ef11e2ca25004bebe
Bereket Abraham <babraham42@gmail.com>
2018-04-19 00:53:19 -0700

    notes

--- commands.sh
+++ commands.sh
@@ -23,10 +23,12 @@ accuracy ~/tigrinya_ocr/eval/tir_groundtruth.txt ~/tigrinya_ocr/tessdata/best/ti
 accuracy ~/tigrinya_ocr/eval/tir_groundtruth_ascii.txt ~/tigrinya_ocr/eval/tir_testdata_ascii-00.txt
 
 # create a new tir.traineddata
-combine_lang_model --input_unicharset ~/tigrinya_ocr/tir/Ethiopic.unicharset \
-  --script_dir ~/tigrinya_ocr/tir --output_dir ~/tigrinya_ocr/training --lang tir \
-  --words ~/tigrinya_ocr/tir/tir.wordlist --puncs ~/tigrinya_ocr/tir/tir.punc --numbers ~/tigrinya_ocr/tir/tir.numbers \
-  --version_str 0.0.1
+combine_lang_model --input_unicharset ~/tigrinya_ocr/langdata/Ethiopic.unicharset \
+  --script_dir ~/tigrinya_ocr/langdata --output_dir ~/tigrinya_ocr/training \
+  --words ~/tigrinya_ocr/langdata/tir/tir.wordlist \
+  --puncs ~/tigrinya_ocr/langdata/tir/tir.punc \
+  --numbers ~/tigrinya_ocr/langdata/tir/tir.numbers \
+  --lang tir --version_str 0.0.1
 
 # list fonts
 fc-list | grep tir_fonts
@@ -35,40 +37,46 @@ text2image --list_available_fonts --fonts_dir /usr/local/share/fonts/tir_fonts
 # update language specific fonts under AMHARIC_FONTS
 sudo vi /usr/share/tesseract-ocr/language-specific.sh
 
+sudo mv /usr/share/tesseract-ocr/4.00/tessdata/tir.traineddata ~/tigrinya_ocr/original
+sudo cp ~/tigrinya_ocr/training/tir/tir.traineddata /usr/share/tesseract-ocr/4.00/tessdata/tir.traineddata
+
 # generate test data
 /usr/share/tesseract-ocr/tesstrain.sh \
-  --fontlist "Abyssinica SIL" "Abyssinica SIL Sebatbeit" "Abyssinica SIL Ximtanga" "Code2003 Medium" "Ethiopia Jiret" "Ethiopic Fantuwua" "Ethiopic Hiwua" "Ethiopic Tint" "Ethiopic WashRa Bold, Bold" "Ethiopic WashRa SemiBold, Bold" "Ethiopic Wookianos" "Ethiopic Yebse" "Ethiopic Zelan" "GF Zemen Unicode" "Ethiopic Yigezu Bisrat Goffer" "Ethiopic Yigezu Bisrat Gothic" \
+  --fontlist "Abyssinica SIL" "Abyssinica SIL Sebatbeit" "Abyssinica SIL Ximtanga" "Code2003 Medium" "Droid Sans Ethiopic" "Droid Sans Ethiopic Bold" "Ethiopia Jiret" "Ethiopic Fantuwua" "Ethiopic Hiwua" "Ethiopic Tint" "Ethiopic WashRa Bold, Bold" "Ethiopic WashRa SemiBold, Bold" "Ethiopic Wookianos" "Ethiopic Yebse" "Ethiopic Zelan" "GF Zemen Unicode" "Noto Sans Ethiopic" "Noto Sans Ethiopic Bold" "Ethiopic Yigezu Bisrat Goffer" "Ethiopic Yigezu Bisrat Gothic" \
   --fonts_dir /usr/local/share/fonts/tir_fonts \
   --lang tir --langdata_dir ~/tigrinya_ocr \
   --output_dir ~/tigrinya_ocr/generated \
   --overwrite --linedata_only --exposures 0 --noextract_font_properties \
   --training_text ~/tigrinya_ocr/eval/tir_groundtruth.txt \
   --wordlist ~/tigrinya_ocr/tir/tir.wordlist \
-  --tessdata_dir ~/tigrinya_ocr/training 
+#  --tessdata_dir ~/tigrinya_ocr/training 
 
-# for only 1 font
-/usr/share/tesseract-ocr/tesstrain.sh \
-  --fontlist "Abyssinica SIL" \
-  --fonts_dir /usr/local/share/fonts/tir_fonts \
-  --lang tir --langdata_dir ~/tigrinya_ocr/langdata \
-  --output_dir ~/tigrinya_ocr/generated \
-  --overwrite --linedata_only --exposures 0 --noextract_font_properties \
-  --training_text ~/tigrinya_ocr/eval/tir_groundtruth.txt \
-  --wordlist ~/tigrinya_ocr/langdata/tir/tir.wordlist \
-  --tessdata_dir ~/tigrinya_ocr/training
+for font in "Abyssinica SIL" "Abyssinica SIL Sebatbeit" "Abyssinica SIL Ximtanga" "Code2003 Medium" "Droid Sans Ethiopic" "Ethiopia Jiret" "Ethiopic Fantuwua" "Ethiopic Hiwua" "Ethiopic Tint" "Ethiopic WashRa Bold, Bold" "Ethiopic WashRa SemiBold, Bold" "Ethiopic Wookianos" "Ethiopic Yebse" "Ethiopic Zelan" "GF Zemen Unicode" "Noto Sans Ethiopic" "Ethiopic Yigezu Bisrat Goffer" "Ethiopic Yigezu Bisrat Gothic"
+do
+  /usr/share/tesseract-ocr/tesstrain.sh \
+    --fontlist "$font" \
+    --fonts_dir /usr/local/share/fonts/tir_fonts \
+    --lang tir --langdata_dir ~/tigrinya_ocr/langdata \
+    --output_dir ~/tigrinya_ocr/generated \
+    --overwrite --linedata_only --exposures 0 --noextract_font_properties \
+    --training_text ~/tigrinya_ocr/eval/tir_groundtruth.txt \
+    --wordlist ~/tigrinya_ocr/langdata/tir/tir.wordlist \
+  #  --tessdata_dir ~/tigrinya_ocr/training
+done
 
 # view generated images
 gksudo nautilus --browser /tmp/tmp.wpk6QANxS5/tir
 xdg-open /tmp/tmp.wpk6QANxS5/tir/tir.Abyssinica_SIL.exp0.tif
 
 # create filnames file
-ls -d $PWD/tigrinya_ocr/generated/*.lstmf > ~/tigrinya_ocr/generated/tir.training_files.txt
+ls -d ~/tigrinya_ocr/generated/*.lstmf > ~/tigrinya_ocr/generated/tir.training_files.txt
 
 # evaluate model
 lstmeval --model ~/tigrinya_ocr/langdata/tir/tir_best.traineddata \
   --eval_listfile ~/tigrinya_ocr/generated/tir.training_files.txt
+# At iteration 0, stage 0, Eval Char error rate=38.163577, Word error rate=37.982017
 
-lstmtraining --model_output ~/tigrinya_ocr/training \
+lstmtraining --model_output ~/tigrinya_ocr/tuning \
   --continue_from ~/tigrinya_ocr/langdata/tir/tir_best.traineddata \
   --traineddata ~/tigrinya_ocr/langdata/tir/tir.traineddata \
   --train_listfile ~/tigrinya_ocr/generated/tir.training_files.txt \
