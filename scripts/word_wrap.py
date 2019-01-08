07fb4b1f0d49bcfcda1212e0aa1cdd1d0ee3e76c
Bereket Abraham <babraham42@gmail.com>
2019-01-08 11:51:34 -0800

    Create word_wrap.py

---
+++ scripts/word_wrap.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# python script to wrap long lines in a text corpus
+
+import textwrap
+
+char_limit = 100
+file_in = '/home/babraham/tigrinya_ocr/raw_data/corpus.txt'
+file_out = 'wrapped_corpus.txt'
+
+def main():
+    wrapper = textwrap.TextWrapper()
+    wrapper.width = char_limit
+    # all whitespace chars are converted to space: '\t\n\v\f\r'
+    wrapper.expand_tabs = true
+    wrapper.replace_whitespace = true
+    wrapper.break_long_words = false
+
+    # read corpus
+    with open(file_in) as f:
+        # text = f.readlines()
+        text = f.read()
+
+    text = text.decode('utf-8')
+    lines = text.splitlines()
+    lines = [wrapper.fill(line) for line in lines]
+    text = '\n'.join(lines)
+    
+    with open(file_out) as f:
+        f.write(text)
+
+
+if __name__ == "__main__":
+    main()
