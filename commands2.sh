05964c3ba5f9819dc4c8c63ea6e99c611c9864e7
Bereket Abraham <babraham42@gmail.com>
2019-01-08 01:46:00 -0800

    Added more notes on install and usage.

--- commands2.sh
+++ commands2.sh
@@ -10,8 +10,15 @@ kraken -i image.tif bw.png binarize
 # segment a binarized image into reading-order sorted lines
 kraken -i bw.png lines.json segment
 
+kraken list
+kraken get default
+ls ~/.config/kraken/
+
 # OCR a binarized image using the default RNN and the previously generated page segmentation
 kraken -i bw.png image.txt ocr --lines lines.json
+
+# generate synthetic data
+ketos linegen -u NFD -f "Abyssinica SIL" corpus.txt
 
 # train a kraken model
 
