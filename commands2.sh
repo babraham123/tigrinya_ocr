c342ed6d2a5c6e843ecf3d1edc3b20aa76f390b5
Bereket Abraham <babraham42@gmail.com>
2019-01-13 22:12:22 -0800

    updated install instructions

--- commands2.sh
+++ commands2.sh
@@ -7,8 +7,8 @@ kraken -i image.tif image.txt binarize segment ocr
 # binarize a single image using the nlbin algorithm
 kraken -i image.tif bw.png binarize
 
-# segment a binarized image into reading-order sorted lines
-kraken -i bw.png lines.json segment
+# segment a binarized image into reading-order sorted lines, with script detection
+kraken -i bw.png lines.json segment -s
 
 kraken list
 kraken get default
@@ -17,8 +17,20 @@ ls ~/.config/kraken/
 # OCR a binarized image using the default RNN and the previously generated page segmentation
 kraken -i bw.png image.txt ocr --lines lines.json
 
+# create a corpus, wrap words
+cat *.txt > corpus.txt
+python scripts/word_wrap.py
+
 # generate synthetic data
 ketos linegen -u NFD -f "Abyssinica SIL" corpus.txt
 
-# train a kraken model
+# predict with voting mechanism
+calamari-predict --checkpoint 0.ckpt --checkpoint 1.ckpt --files *.png
+
+# evaluate predictions
+calamari-eval --gt *.gt.txt
+
+# train a calamari model
+calamari-cross-fold-train --files training_data/*.png --best_models_dir models --temporary_dir tmp &> training.log
+
 
