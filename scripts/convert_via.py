f64aa029d9f7c7a9bb66b6b20d1703b1d63830a8
Bereket Abraham <babraham42@gmail.com>
2019-04-01 01:12:23 -0700

    mostly done via script

--- scripts/convert_via.py
+++ scripts/convert_via.py
@@ -6,39 +6,31 @@
 from dh_segment.io import via
 
 collection = 'mycollection'
-annotation_file = 'via_sample.json'
-masks_dir = '/home/project/generated_masks'
-images_dir = './my_images'
-
-# Load all the data in the annotation file
-# (the file may be an exported project or an export of the annotations)
-via_data = via.load_annotation_data(annotation_file)
-
-# In the case of an exported project file, you can set ``only_img_annotations=True``
-# to get only the image annotations
-via_annotations = via.load_annotation_data(annotation_file, only_img_annotations=True)
-
-# Collect the annotated regions
-working_items = via.collect_working_items(via_annotations, collection, images_dir)
-
-# Collect the attributes and options
-if '_via_attributes' in via_data.keys():
-    list_attributes = via.parse_via_attributes(via_data['_via_attributes'])
-else:
-    list_attributes = via.get_via_attributes(via_annotations)
-
-# Create one mask per option per attribute
-via.create_masks(masks_dir, working_items, list_attributes, collection)
-
+annotation_file = 'via_annontations.json'
+masks_dir = './masks'
+images_dir = './train'
 
 def main():
-    if len(sys.argv) != 3:
-        print('Incorrect arguments!')
-        exit()
+    # Load all the data in the annotation file
+    # (the file may be an exported project or an export of the annotations)
+    # via_data = via.load_annotation_data(annotation_file)
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    # In the case of an exported project file, you can set ``only_img_annotations=True``
+    # to get only the image annotations
+    via_annotations = via.load_annotation_data(annotation_file, only_img_annotations=True)
 
+    # Collect the annotated regions
+    working_items = via.collect_working_items(via_annotations, collection, images_dir)
+
+    # Collect the attributes and options
+    if '_via_attributes' in via_data.keys():
+        list_attributes = via.parse_via_attributes(via_data['_via_attributes'])
+    else:
+        list_attributes = via.get_via_attributes(via_annotations)
+
+    # Create one mask per option per attribute
+    via.create_masks(masks_dir, working_items, list_attributes, collection)
+    print('Created masks. {} regions, {} attributes.'.format(len(working_items), len(list_attributes)))
 
 
 if __name__ == '__main__':
